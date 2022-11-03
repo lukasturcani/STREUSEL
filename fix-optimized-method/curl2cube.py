e57aa84b972f2e2439f00dd6a09c7e5b9fb47a69
Lukas Turcani <lukasturcani@mailbox.org>
2022-11-03 19:18:37 +0000

    Clean up script

--- fix-optimized-method/curl2cube.py
+++ fix-optimized-method/curl2cube.py
@@ -3,54 +3,60 @@ Write the curl of the electrostatic potential to a cube file.
 
 """
 
-import streusel
-import glob
-import pathlib
 import argparse
-import numpy as np
-from write_cube import write_cube
+import pathlib
+
 import matplotlib.pyplot as plt
+from write_cube import write_cube
+
+import streusel
 
 
 def _get_command_line_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("neutral_atom_directory")
-    parser.add_argument('-a', '--atom')
-    parser.add_argument("output_path")
+    parser = argparse.ArgumentParser(
+        description=(
+            "Write the curl of the electrostatic potential to a cube file."
+        ),
+    )
+    parser.add_argument(
+        "cube_file",
+        type=pathlib.Path,
+        help="A .cube file holding the electrostatic potential.",
+    )
+    parser.add_argument(
+        "output_folder",
+        help="The path to the folder into which the output should be written.",
+        type=pathlib.Path,
+    )
     return parser.parse_args()
 
 
 def main() -> None:
-
     cli_args = _get_command_line_arguments()
-    print(cli_args.neutral_atom_directory)
-    print(cli_args.atom)
+    cli_args.output_folder.mkdir(parents=True, exist_ok=True)
 
-    neutral_atom_directory = pathlib.Path(cli_args.neutral_atom_directory)
+    atom = streusel.Molecule(cli_args.cube_file)
+    atom.get_efield()
+    atom.sample_efield_optimized()
 
-    if cli_args.atom == None:
-        globbed_neutral_atom_directories_to_scan = neutral_atom_directory.joinpath('*/epot_200.cube')
-    else:
-        globbed_neutral_atom_directories = neutral_atom_directory.joinpath(f'{cli_args.atom}_neut/epot_200.cube')
+    for x in range(75, 125):
+        plt.imshow(atom.efield[:, :, x], cmap="viridis")
+        plt.colorbar()
+        plt.savefig(cli_args.output_folder / f"efield_{x}.png")
 
-    for atom_cube_file in glob.glob(str(globbed_neutral_atom_directories)):
-        atom = streusel.Molecule(atom_cube_file)
-        atom.get_efield()
-        atom.sample_efield_optimized()
-        curl = atom.optimized_method_curl
-        efield = atom.efield
+        plt.imshow(atom.optimized_method_curl[:, :, x], cmap="viridis")
+        plt.colorbar()
+        plt.savefig(cli_args.output_folder / f"curl_{x}.png")
 
-        for x in range(75,125):
-            plt.imshow(efield[:,:,x], cmap='viridis')
-            plt.colorbar()
-            plt.savefig(f'efield_{x}.png')
-
-            plt.imshow(curl[:,:,x], cmap='viridis')
-            plt.colorbar()
-            plt.savefig(f'curl_{x}.png')
-
-        write_cube(cli_args.output_path, curl, atom.coords, atom.atoms.symbols, atom.origin, atom.res)
+    write_cube(
+        cli_args.output_folder / "curl.cube",
+        atom.optimized_method_curl,
+        atom.coords,
+        atom.atoms.symbols,
+        atom.origin,
+        atom.res,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- fix-optimized-method/write_cube.py
+++ fix-optimized-method/write_cube.py
@@ -1,44 +1,70 @@
+from collections import abc
+
 import numpy as np
-import itertools
+
 
 def write_cube(
-    filepath:str,
-    vox_grid:np.ndarray,
-    coords:np.ndarray,
-    elements:list[str],
+    path: str,
+    voxel_grid: np.ndarray,
+    atomic_coordinates: np.ndarray,
+    elements: abc.Collection[str],
     origin: np.ndarray,
-    res: np.ndarray,
+    resolution: np.ndarray,
 ) -> None:
-    origin_x, origin_y, origin_z = origin
-    res_x, res_y, res_z = res
-    nvox_x, nvox_y, nvox_z = vox_grid.shape
-    with open(filepath, 'w') as cube:
-        # write header stuff
-        lines = [
-            ' title',
-            ' title2',
-            f'{len(elements): >5} {origin_x: >11.6f} {origin_y: >11.6f} {origin_z: >11.6f}',
-            f'{nvox_x: >5} {res_x: >11.6f} {0.: >11.6f} {0.: >11.6f}',
-            f'{nvox_y: >5} {0.: >11.6f} {res_y: >11.6f} {0.: >11.6f}',
-            f'{nvox_z: >5} {0.: >11.6f} {0.: >11.6f} {res_z: >11.6f}',
-        ]
-        for element, [x, y, z] in zip(elements, coords):
-            lines.append(f'{element: >5} {0.: >11.6f} {x: >11.6f} {y: >11.6f} {z: >11.6f}')
+    """
+    Write a ``.cube`` file.
 
-        # write voxel grid to cube file from tensor
+    Parameters:
+        path:
+            The path to the file.
+        voxel_grid:
+            The voxel values to be written to the cube file.
+        atomic_coordinates:
+            An N by 3 position matrix written to the cube file.
+        elements:
+            For each atom, its elemental symbol.
+        origin:
+            The origin field for the cube file.
+        resolution:
+            An array of size 3, holding length of voxels in each
+            dimension.
+
+    """
+
+    origin_x, origin_y, origin_z = origin
+    res_x, res_y, res_z = resolution
+    nvox_x, nvox_y, nvox_z = voxel_grid.shape
+    with open(path, "w") as cube:
+        lines = [
+            " title",
+            " title2",
+            (
+                f"{len(elements): >5} {origin_x: >11.6f} "
+                f"{origin_y: >11.6f} {origin_z: >11.6f}"
+            ),
+            f"{nvox_x: >5} {res_x: >11.6f} {0.: >11.6f} {0.: >11.6f}",
+            f"{nvox_y: >5} {0.: >11.6f} {res_y: >11.6f} {0.: >11.6f}",
+            f"{nvox_z: >5} {0.: >11.6f} {0.: >11.6f} {res_z: >11.6f}",
+        ]
+        for element, [x, y, z] in zip(elements, atomic_coordinates):
+            lines.append(
+                f"{element: >5} {0.: >11.6f} "
+                f"{x: >11.6f} {y: >11.6f} {z: >11.6f}"
+            )
+
         ncols = 6
         for i in range(nvox_x):
             for j in range(nvox_y):
                 line = []
                 for k in range(nvox_z):
-                    line.append(f'{vox_grid[i, j, k]: >12.5E}')
+                    line.append(f"{voxel_grid[i, j, k]: >12.5E}")
                     if len(line) == ncols:
                         lines.append(_pad_left(" ".join(line)))
                         line = []
                 lines.append(_pad_left(" ".join(line)))
                 line = []
-        content = '\n'.join(lines)
-        cube.write(f'{content}\n')
+        content = "\n".join(lines)
+        cube.write(f"{content}\n")
 
 
 def _pad_left(s: str) -> str:
